c0a624e24d40d0fc3bda88097aa611944713bfca
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-24 20:33:49 +0200

    loadArray(Client *&arr, int &size)

--- Client.cpp
+++ Client.cpp
@@ -21,7 +21,23 @@ void saveArray(Client *arr, int size) {
 }
 
 void loadArray(Client *&arr, int &size) {
-
+    FILE* fp = nullptr;
+    // fopen_s(&fp, "bank.txt", "r"); //VS
+    fp =  fopen("bank.txt", "r"); //Clion, Online
+    if (fp == nullptr) {
+        arr = nullptr;
+        size  = 0;
+        cout << "Error: not loaded!\n";
+    }else{
+       // fscanf_s(fp, "%d", &size); // VS
+        fscanf(fp, "%d", &size); // Clion, Online
+        arr = new Client[size];
+        for (int i = 0; i < size; i++) {
+            arr[i].loadFromTextFile(fp);
+        }
+        fclose(fp);
+        cout << "Loaded!\n";
+    }
 }
 
 void printArray(Client *arr, int size) {
