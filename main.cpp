3e82137331078eb04180439e9184311fb8e2cb63
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-24 20:07:40 +0200

    main

--- main.cpp
+++ main.cpp
@@ -2,14 +2,33 @@
 
 int main() {
 
+    /*
     Client a;
     a.fillClient();
-    a.money += 200;
-
 
     cout << "\n\n";
     a.printClient();
 
+    FILE* fp = nullptr;
+    //fopen_s(&fp, "bank.txt", "w"); //VS
+    fp = fopen( "bank.txt", "w"); //CLion, Online
+    if (fp!= nullptr) {
+        a.saveToTextFile(fp);
+        fclose(fp);
+        cout << "Saved!\n";
+    }*/
+
+    Client b; // name = "", number = 0, money = 0, ...
+
+    FILE* fp = nullptr;
+    //fopen_s(&fp, "bank.txt", "r"); //VS
+    fp = fopen("bank.txt", "r"); //CLion, Online
+    if (fp != nullptr) {
+        b.loadFromTextFile(fp);
+        fclose(fp);
+    }
+
+    b.printClient();
 
     return 0;
 }
