1dc37ae296dcafc4e06cdc66f1ebc56a918f9036
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-24 19:45:24 +0200

    loadFromTextFile

--- Client.h
+++ Client.h
@@ -36,9 +36,20 @@ struct Client {
 
     void saveToTextFile(FILE* fp) {
         fprintf(fp, "\n%s", name);
-        fprintf(fp, "\n%ul", number);
+        fprintf(fp, "\n%lu", number);
         fprintf(fp, "\n%lf", money);
         fprintf(fp, "\n%d %d %d", opened_date.year, opened_date.month, opened_date.day);
+    }
+
+    void loadFromTextFile(FILE* fp) {
+        fscanf_s(fp, "\n"); //VS
+        fgets(name, 100, fp); //"Pupkin Mykola\n"
+        int len = strlen(name);
+        name[len - 1] = '\0';   //"Pupkin Mykola\0"
+
+        fscanf_s(fp, "%lu", &number);
+        fscanf_s(fp, "%lf", &money);
+        fscanf_s(fp, "%d %d %d", &opened_date.year, &opened_date.month, &opened_date.day);
     }
 };
 
