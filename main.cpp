c5f7d10da8182c99065eff2a3455404a7a4a9b4d
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-24 20:15:06 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Client.h"
+#define CREATE_OR_LOAD 0 //0-Create, 1-Load
 
 int main() {
 
@@ -18,7 +19,7 @@ int main() {
         cout << "Saved!\n";
     }*/
 
-    Client b; // name = "", number = 0, money = 0, ...
+    /*Client b; // name = "", number = 0, money = 0, ...
 
     FILE* fp = nullptr;
     //fopen_s(&fp, "bank.txt", "r"); //VS
@@ -28,7 +29,24 @@ int main() {
         fclose(fp);
     }
 
-    b.printClient();
+    b.printClient();*/
+
+    int NClients = 0; //К-ть клієнтів = Розмір масиву
+    Client* arr = nullptr; //Масив клієнтів
+
+#if CREATE_OR_LOAD == 0
+    NClients = 3;
+    arr = new Clien[NClients] {
+        {"Pupkin Mykola", 123456, 200, {1, 1, 2000}},
+        {"Shevchenko Taras", 543216, 5'000, {12, 7, 2021}},
+        {"Franko Ivan", 765432, 2'500, {30, 8, 2019}}
+    };
+#elif CREATE_OR_LOAD == 1
+    //load
+#endif
+
+
+
 
     return 0;
 }
